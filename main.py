2836a29a5df3b02c9cb9cd6a4c49209aaffa648f
John H <65785906+ZenjiOP@users.noreply.github.com>
2024-11-13 19:45:05 -0800

    Update main.py
    added a short note from in class discussion

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+if __name__ == '__main__':
+    pass
 
+'''Pseudocode / Notes =========================
+sort the tasks into a tree, with each task as a node and its dependencies represented as an edge linking nodes.
+'''
